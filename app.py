a1a5c774f168897b1ced693e988059510b56c1da
gayathrip25it-collab <gayathri.p_25it@kgkite.ac.in>
2026-08-19 18:34:26 +0530

    Configure production server

--- app.py
+++ app.py
@@ -629,4 +629,4 @@ def seed_initial_data(app):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
